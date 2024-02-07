2a148186b0c683fb261ea6b1a81781f429c619bb
Orhan Dogan <orhan1766@gmail.com>
2024-02-07 23:53:16 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager:ISupplierService
+    {
+        private IApplicantService _aplicantService;
+
+        public PttManager(IApplicantService aplicantService)
+        {
+            _aplicantService = aplicantService;
+        }
+
+        public void GiveMask(Person person)
+        {
+            if (_aplicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine("Maske verilemedi.");
+            }
+        }
+    }
+}
---
+++ WorkAround/Program.cs
@@ -0,0 +1,15 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person1 = new Person { FirstName = "Orhan", LastName = "Doğan", DateOfBirth = 1990, NationalIdentity = 12345678910 };
+
+        PersonManager personManager = new PersonManager();
+        PttManager pttManager = new PttManager(personManager);
+        pttManager.GiveMask(person1);
+    }
+}
